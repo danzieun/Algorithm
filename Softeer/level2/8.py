a49e6770d00768d42ea446e48463ca71b8989e90
jjieunaa <jieun3744@gmail.com>
2023-03-02 11:04:29 +0900

    230301

---
+++ Softeer/level2/8.py
@@ -0,0 +1,10 @@
+import sys
+
+a = list(map(int, sys.stdin.readline().split()))
+
+if a == sorted(a):
+    print('ascending')
+elif a == sorted(a, reverse=True):
+    print('descending')
+else:
+    print('mixed')
